81f73be197509671eb8dde14a367d7bb6e9be941
xuyujie <1264116203@qq.com>
2022-06-19 15:01:18 +0800

    feat: 计算各省份各线路日均流量

--- src/components/calculate.vue
+++ src/components/calculate.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
     <a-form-model ref="ruleForm" :model="form" :rules="rules" :label-col="{ span: 2 }" :wrapper-col="{ span: 6 }">
-      <a-form-model-item label="省份" prop="province" >
+      <a-form-model-item label="省份" prop="province">
         <a-select v-model="form.province" placeholder="请选择省份">
           <a-select-option :value="item" v-for="(item,index) in provinces" :key="index">
-            {{item}}
+            {{ item }}
           </a-select-option>
         </a-select>
       </a-form-model-item>
       <a-form-model-item label="年度" prop="year">
         <a-select v-model="form.year" :default-value="years[0].value" placeholder="请选择年度">
-          <a-select-option :value="item.value" v-for="(item,index) in years" :key="index" >
+          <a-select-option :value="item.value" v-for="(item,index) in years" :key="index">
             {{ item.label }}
           </a-select-option>
         </a-select>
@@ -19,26 +19,26 @@
         <a-input v-model="form.TrafficGrowthMult" placeholder="请填写流量增长倍数（%）" />
       </a-form-model-item>
       <a-form-model-item label="4G小区信息表">
-      <a-space>
-        <a-upload
-            v-model="uploadFile"
-            :accept="accept"
-            :file-list="fileList"
-            :before-upload="beforeUpload"
-            :remove="remove"
-        >
-          <a-button>
-            <a-icon type="upload" />
-            点击上传
+        <a-space>
+          <a-upload
+              v-model="uploadFile"
+              :accept="accept"
+              :file-list="fileList"
+              :before-upload="beforeUpload"
+              :remove="remove"
+          >
+            <a-button>
+              <a-icon type="upload" />
+              点击上传
+            </a-button>
+          </a-upload>
+          <a-button type="primary" @click="calculate">
+            计算
           </a-button>
-        </a-upload>
-        <a-button type="primary" @click="calculate">
-          计算
-        </a-button>
-        <a-button type="primary" @click="exportData">
-          导出
-        </a-button>
-      </a-space>
+          <a-button type="primary" @click="exportData">
+            导出
+          </a-button>
+        </a-space>
       </a-form-model-item>
     </a-form-model>
     <a-table :columns="columns" :data-source="data">
@@ -60,30 +60,30 @@ export default {
       /** 上传的文件列表 */
       fileList: [],
       /** 省份 */
-      provinces: ['黑龙江','辽宁','吉林','河北','山西','陕西','山东','江苏','浙江','河南','湖北','安徽','湖南','江西','云南','海南','四川','台湾','贵州','广东','甘肃','青海','福建'],
-      defaultValue:"2022",
+      provinces: ['黑龙江', '辽宁', '吉林', '河北', '山西', '陕西', '山东', '江苏', '浙江', '河南', '湖北', '安徽', '湖南', '江西', '云南', '海南', '四川', '台湾', '贵州', '广东', '甘肃', '青海', '福建'],
+      defaultValue: '2022',
       /** 年份 */
       years: [
         {
-          label:"2018",
-          value:"2018"
+          label: '2018',
+          value: '2018'
         },
         {
-          label:"2019",
-          value:"2019"
+          label: '2019',
+          value: '2019'
         },
         {
-          label:"2020",
-          value:"2020"
+          label: '2020',
+          value: '2020'
         },
         {
-          label:"2021",
-          value:"2021"
+          label: '2021',
+          value: '2021'
         },
         {
-          label:"2022",
-          value:"2022"
-        },
+          label: '2022',
+          value: '2022'
+        }
       ],
       /** 增长倍率 */
       growthRate: 0,
@@ -91,17 +91,15 @@ export default {
       testSheetData: [],
       /** 基本信息表数据 */
       baseInfoSheetData: [],
-      /** 竞对信息表数据 */
-      rivalInfoSheetData: [],
       form: {
         province: undefined,
         year: undefined,
         TrafficGrowthMult: 0
       },
-      rules:{
-      province: [{ required: true, message: '请选择省份', trigger: ['change','blur'] }],
-      year: [{ required: true, message: '请选择年度', trigger: ['change','blur'] }],
-      TrafficGrowthMult: [{ required: true, message: '请填写流量增长倍数', trigger: ['change','blur'] }],
+      rules: {
+        province: [{ required: true, message: '请选择省份', trigger: ['change', 'blur'] }],
+        year: [{ required: true, message: '请选择年度', trigger: ['change', 'blur'] }],
+        TrafficGrowthMult: [{ required: true, message: '请填写流量增长倍数', trigger: ['change', 'blur'] }]
       },
       columns: [
         {
@@ -130,7 +128,9 @@ export default {
           key: '700MHz'
         }
       ],
-      data: []
+      data: [],
+      /** 各省份下线路构成归属的日均流量总和 */
+      provinceLineFlowSum: 0
 
     }
   },
@@ -147,6 +147,7 @@ export default {
           try {
 
             const data = ev.target.result
+
             const workbook = XLSX.read(data, {
               type: 'binary'
             })
@@ -156,17 +157,21 @@ export default {
                   this.baseInfoSheetData = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName])
                   break
                 case '测试数据':
-                  this.testSheetData = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName])
-                  break
-                case '竞对信息':
-                  this.rivalInfoSheetData = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName])
+                  this.testSheetData = this.testSheetData.concat(XLSX.utils.sheet_to_json(workbook.Sheets[sheetName],
+                      {
+                        header: [
+                          'b', 'c', 'd', 'e', 'f', 'g', 'h',
+                          'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+                          'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                          'aa', 'ab'
+                        ]
+                      }))
                   break
               }
 
             }
             console.log('基本信息表', this.baseInfoSheetData)
             console.log('测试数据', this.testSheetData)
-            console.log('竞对信息', this.rivalInfoSheetData)
           } catch (e) {
             return false
           }
@@ -189,13 +194,33 @@ export default {
     calculate() {
       this.$refs.ruleForm.validate(valid => {
         if (valid) {
-          console.log("通过校验")
+          console.log('通过校验')
+          let provinceLineFlowSumMap = new Map()
+          this.testSheetData.shift()
+          console.log('测试数据', this.testSheetData)
+
+          this.testSheetData.filter(item => {
+            return item.c === this.form.province
+          }).map(item => {
+
+            if (!provinceLineFlowSumMap.has(item.f)) {
+              provinceLineFlowSumMap.set(item.f, item)
+            } else {
+              debugger
+              let newVar = provinceLineFlowSumMap.get(item.f)
+              newVar.ab = parseFloat(newVar.ab)
+              newVar.ab += parseFloat(item.ab)
+              newVar.ab = newVar.ab.toFixed(2)
+              console.log('newVar:', newVar)
+            }
+          })
+          console.log('provinceLineFlowSumMap:', provinceLineFlowSumMap)
+
+
         } else {
-          return false;
+          return false
         }
-      });
-
-
+      })
 
 
     },
@@ -209,7 +234,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.ant-form{
+.ant-form {
   margin: 20px;
 }
 </style>
